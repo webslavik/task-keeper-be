c398b03c589e233b82d6c115d42ac5a4fc8ce6aa
Slavik <vsukhanov@healthjoy.com>
2023-07-30 14:44:55 +0300

    add DB_NAME to env variables

--- src/constants.py
+++ src/constants.py
@@ -7,6 +7,7 @@ load_dotenv()
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 JWT_ALGORITHM = os.getenv("ALGORITHM")
--- src/db_setup.py
+++ src/db_setup.py
@@ -3,10 +3,10 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 
-from src.constants import DB_USER, DB_PASSWORD, DB_HOST
+from src.constants import DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/task_keeper_db"
+SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL,
